d4b5b3ecf624ab63aebaf991219ac7495d21f9ec
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-11 22:42:53 +0000

    added additional error handling

--- download_avatars.js
+++ download_avatars.js
@@ -1,19 +1,38 @@
 // Import modules
-require('dotenv').config();
+const env = require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 
 // Command line arguments for repository to stream from
-var repoOwner = process.argv[2];
-var repoName = process.argv[3];
+var cmdInput = process.argv.slice(2);
+var repoOwner = cmdInput[0];
+var repoName = cmdInput[1];
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+// dotenv error handling
+if (env.error || process.env.GITHUB_TOKEN === undefined) {
+    console.log("Error! Check to see if your .env file exists.");
+    console.log("If so, check your token credentials.\n" );
+    throw env.error;
+};
+
+// Require two command line arguments to start function
+if (cmdInput.length === 2 && repoOwner !== undefined && repoName !== undefined) {
+} else {
+    console.log("Process terminated. A valid repo owner and name are required")
+    console.log("Any additional inputs are not valid.");
+    return;
+};
+
+//////////////* START GITHUB AVATAR DOWNLOADING *//////////////
+
+console.log('Welcome to the GitHub Avatar Downloader! \n');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
         headers: {
-            'User-Agent': process.env.GITHUB_USER,
+            'User-Agent': 'request',
             'Authorization': 'token ' + process.env.GITHUB_TOKEN,
         }
     };
@@ -23,7 +42,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
             throw err;
         };
         if (res.statusCode !== 200) {
-            console.log('Status: ' + res.statusCode + '. Connection problem! Please check your inputs!');
+            console.log('Status: ' + res.statusCode + '. Connection problem! Please check your token or repos!');
         }
         // Parse through text stream and store in data object
         var data = JSON.parse(body);
@@ -35,6 +54,7 @@ function downloadImageByURL(url, filePath) {
         
     request.get(url)
         .on('error', function (err) {
+            console.log("Error! Invalid URL!");
             throw err;
         })
         .on('response', function (response) {
@@ -43,30 +63,22 @@ function downloadImageByURL(url, filePath) {
         })
         // Write images to avatar folder
         .pipe(fs.createWriteStream('./avatar/' + filePath + '.jpg'))
-        .on('finish', function () {
+        .on('finish', function (){
             console.log('Download complete.');
         });
 }
 
-// Require both command line arguments to start function
-if (repoOwner !== undefined && repoName !== undefined) {
-    getRepoContributors(repoOwner, repoName, function (err, result) {
-        console.log("Errors:", err);
-
-        // Create a directory called avatar if one does not exist.
-        var directory = './avatar';
-        if (!fs.existsSync(directory)){
-            fs.mkdirSync(directory);
-        }
-        
-        // Iterating parsed data and image download function; saving all avatar_url and login names
-        for (var i = 0; i < result.length; i++) {
-            var url = result[i].avatar_url;
-            var name = result[i].login;
-            downloadImageByURL(url, name);
-        };
-    });
-} else {
-    console.log("Process terminated. Please provide both a valid repository owner and name.");
-    return;
-};
+getRepoContributors(repoOwner, repoName, function (err, result) {
+    // Create a directory called avatar if one does not exist.
+    var directory = './avatar';
+    if (!fs.existsSync(directory)){
+        console.log("Avatar directory not found... Creating directory...")
+        fs.mkdirSync(directory);
+    }
+    // Iterating parsed data and image download function; saving all avatar_url and login names
+    for (var i = 0; i < result.length; i++) {
+        var url = result[i].avatar_url;
+        var name = result[i].login;
+        downloadImageByURL(url, name);
+    };
+});
