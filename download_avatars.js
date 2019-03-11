61e3f1bb9f1d16c22c9c8e4c2c1dd9f95e14f6e8
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-11 20:40:22 +0000

    added some comments in download_avatars.

--- download_avatars.js
+++ download_avatars.js
@@ -1,56 +1,59 @@
+// Import modules
 var request = require('request');
 var fs = require('fs');
 var token = require('./secrets.js');
 
-
-//Require command line input for repo owner and name
+// Command line arguments for repository to stream from
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+console.log('Welcome to the GitHub Avatar Downloader!');
+
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
         headers: {
             'User-Agent': 'request',
-            'Authorization': 'token ' + token.GITHUB_TOKEN,
+            'Authorization': token.GITHUB_TOKEN,
         }
     };
-    //Check for errors and status code during request
+    // Check for errors and status code during request
     request(options, function(err, res, body) {
         if (err) {
             throw err;
         };
         if (res.statusCode !== 200) {
-            console.log('Status: ' + res.statusCode + '. Connection problem! Check your inputs!');
+            console.log('Status: ' + res.statusCode + '. Connection problem! Please check your inputs!');
         }
-        //Parse through body of text and store in data object
+        // Parse through text stream and store in data object
         var data = JSON.parse(body);
         cb(err, data);
         });
 }
 
 function downloadImageByURL(url, filePath) {
-        request.get(url)
-            .on('error', function (err) {
-                throw err;
-            })
-            
-            .on('response', function (response) {
-                console.log('Downloading image...');
-                console.log('Response Status Code: ', response.statusCode, response.headers['content-type']);
-            })
-            
-            .pipe(fs.createWriteStream('./avatar/' + filePath + '.jpg'))
-            .on('finish', function () {
-                console.log('Download complete.');
-            });
+        
+    request.get(url)
+        .on('error', function (err) {
+            throw err;
+        })
+        .on('response', function (response) {
+            console.log('Downloading image...');
+            console.log('Response Code: ', response.statusCode, response.headers['content-type']);
+        })
+        // Write images to avatar folder
+        .pipe(fs.createWriteStream('./avatar/' + filePath + '.jpg'))
+        .on('finish', function () {
+            console.log('Download complete.');
+        });
 }
 
+// Require both command line arguments to start function
 if (repoOwner !== undefined && repoName !== undefined) {
-    console.log('Welcome to the GitHub Avatar Downloader!');
-    
     getRepoContributors(repoOwner, repoName, function (err, result) {
-        //Looping the data and logging each avatar_url and login entry
+        console.log("Errors:", err);
+        
+        // Looping the data and logging each avatar_url and login entry
         for (var i = 0; i < result.length; i++) {
             var url = result[i].avatar_url;
             var name = result[i].login;
